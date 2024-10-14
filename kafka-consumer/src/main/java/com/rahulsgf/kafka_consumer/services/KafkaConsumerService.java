c0a30649ba93029a974a460bc9c82cbcc30d8660
Rahul Gaur <rgaur1620@gmail.com>
2024-10-14 15:01:00 +0530

    added message receive time in logs

--- kafka-consumer/src/main/java/com/rahulsgf/kafka_consumer/services/KafkaConsumerService.java
+++ kafka-consumer/src/main/java/com/rahulsgf/kafka_consumer/services/KafkaConsumerService.java
@@ -3,6 +3,7 @@ package com.rahulsgf.kafka_consumer.services;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import java.util.Date;
 
 @Service
 @Slf4j
@@ -10,6 +11,10 @@ public class KafkaConsumerService {
 
     @KafkaListener(topics = "test-topic", groupId = "test-consumer-group")
     public void consume(String message) {
-        log.info("Consumed message: {}", message);
+
+        long currentMillis = System.currentTimeMillis();
+        Date receiveTime = new Date(currentMillis);
+
+        log.info("Consumed message: {} at: {}", message, receiveTime);
     }
 }
